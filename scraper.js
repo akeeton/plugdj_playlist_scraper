ba3653acc8da35800abc54a3ef309a4e0d8e402c
Andrew Keeton <andrewrkeeton@gmail.com>
2015-09-18 23:37:10 -0400

    Get floating text to work

--- scraper.js
+++ scraper.js
@@ -37,13 +37,41 @@
 
         document.getElementsByTagName("head")[0].appendChild(styleLink);
     }
+
+
+    //function injectDialog() {
+    //    var dialog = $('<div/>', {
+    //        class: 'dialog',
+    //        title: 'Playlist scrape results'
+    //    });
+
+    //    dialog.appendTo($(document.body));
+    //    dialog.dialog();
+    //}
 	
+                        //#frame iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }
+					//<iframe src='http://en.wikipedia.org/w/index.php?&search="+s+"' onload="$('#frame iframe').slideDown(500);">Enable iFrames.</iframe>
 
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
 		    alert("initMyBookmarklet() 1");
 
-		    alert($(".author").text());
+		    //injectDialog();
+		    //$("#dialog").html(($(".author")));
+
+		    $("body").append(`
+				<div id='scraped_playlists_window'>
+					<div id='scraped_playlists' style=''>
+						<p>Container</p>
+					</div>
+					<style type='text/css'>
+                        #scraped_playlists { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255); cursor: pointer; z-index: 900; }
+                        #scraped_playlists p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }
+					</style>
+				</div>`
+            );
+
+		    $("#scraped_playlists").fadeIn(750);
 		})();
 	}
 })();
